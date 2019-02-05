f09573c96b91f22c02538b5d4c485338440cc31c
rdunavant0 <rdunavant0@gmail.com>
2019-02-05 11:59:00 -0600

    message

---
+++ P1.js
@@ -0,0 +1,13 @@
+//Create button with "Click Me".
+//Once clicked ask the user if they want to continue being asked to quit.
+//If user enters 'n', stop asking.
+//If user enters anything else, stop asking.
+
+function clickMeButtonClickFunction(e)
+var clickMeButtonElement = document.clickMeButtonClickFunction;
+clickMeButtonElement.onclick = clickMeButtonClickFunction;
+var userInput = input("Do you want to continue being asked to quit?");
+if userInput="n"
+console.log()
+if userInput not = "n"
+var userInput = input("Do you want to continue being asked to quit?");
---
+++ P2.js
@@ -0,0 +1,7 @@
+//Use code from Problem 1
+//When user enters 'n', print number of times they did not enter 'n'.
+
+userInputN.textContent = "n";
+var timesNnotEntered = document.getElementsbyName("userInput");
+for(var i=0; 0<=100; i++)
+console.log(timesNnotEntered)
